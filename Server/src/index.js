77f9817a062fd88359453d69070f82d682a5ac37
elianbenjamin <elianbenjamin30@gmail.com>
2023-09-20 18:57:21 -0300

    Merge branch 'main' of https://github.com/elianbenjamin/proyectoIntegrador

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,7 +1,8 @@
 const server = require('./app')
+const { conn } = require('./DB_connection');
 const PORT = 3001;
  
-
+conn.sync({force:true})
 server.listen(PORT, () => {
     console.log('Server raised in port: ' + PORT);})
 
